d62506b2fa2a80b5b6d688249b145f1978f817ec
DVNst <dvn-studio@mail.ru>
2019-08-04 21:56:33 +0300

    updated the build folder on docs/

--- Readme.md
+++ Readme.md
@@ -1,1 +1,15 @@
-Piroll 
+Piroll
+
+---
+
+used by Gulp 3.9
+
+```
+npm install
+
+gulp build
+
+gulp server
+```
+
+---
--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,100 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var css_sorting = require("postcss-sorting");
-var csso = require("gulp-csso");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var css_sorting = require('postcss-sorting');
+var csso = require('gulp-csso');
+var posthtml = require('gulp-posthtml');
+var include = require('posthtml-include');
 
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
 
-var server = require("browser-sync").create();
-var run = require("run-sequence");
+var server = require('browser-sync').create();
+var run = require('run-sequence');
 
-var rename = require("gulp-rename");
-var del = require("del");
+var rename = require('gulp-rename');
+var del = require('del');
+var dir = 'docs';
 
-gulp.task("css", function () {
-  console.log('CSS')
-  return gulp.src("source/sass/style.scss")
+gulp.task('css', () =>
+  gulp.src('source/sass/style.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer(),
       css_sorting()
     ]))
-    .pipe(gulp.dest("css"))
-  .pipe(csso())
-  .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("css"))
-    .pipe(server.stream());
-});
+    .pipe(gulp.dest(dir + '/css'))
+    .pipe(csso())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest(dir + '/css'))
+    .pipe(server.stream())
+);
 
-gulp.task("html", function() {
-  console.log('HTML')
-  return gulp.src("source/*.html")
+gulp.task('html', () =>
+  gulp.src('source/*.html')
     .pipe(posthtml([
       include()
     ]))
-    .pipe(gulp.dest("."));
-});
+    .pipe(gulp.dest(dir))
+);
 
-gulp.task("images", function() {
-  console.log('images')
-  return gulp.src("source/img/*.{png,jpg,svg}")
+gulp.task('images', () =>
+  gulp.src('source/img/*.{png,jpg,svg}')
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("img"))
-});
+    .pipe(gulp.dest(dir + '/img'))
+);
 
-gulp.task("webp", function() {
-  console.log('webp')
-  return gulp.src("source/img/**/*.{png,jpg}")
+gulp.task('webp', () =>
+  gulp.src('source/img/**/*.{png,jpg}')
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("img"))
-});
+    .pipe(gulp.dest(dir + '/img'))
+);
 
-gulp.task("copy", function() {
-  console.log('copy')
-  return gulp.src([
-      "source/fonts/**",
-      "source/img/**",
-      "source/js/**"
+gulp.task('copy', () =>
+  gulp.src([
+      'source/fonts/**',
+      'source/img/**',
+      'source/js/**'
     ], {
-      base: "source"
+      base: 'source'
     })
-    .pipe(gulp.dest("."));
-});
+    .pipe(gulp.dest(dir))
+);
 
-gulp.task("server", function () {
+gulp.task('clean', () =>
+  del(dir)
+);
+
+gulp.task('server', function () {
   server.init({
-    server: ".",
+    server: dir,
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", ["css"]);
-  gulp.watch("source/*.html",  ["html"]).on("change", server.reload);
+  gulp.watch('source/sass/**/*.{scss,sass}', ['css']);
+  gulp.watch('source/*.html',  ['html']).on('change', server.reload);
 });
 
-gulp.task("build", function(done) {
+gulp.task('build', function(done) {
   run(
-    "copy",
-    "images",
-    "webp",
-    "css",
-    "html",
+    'clean',
+    'copy',
+    'images',
+    'webp',
+    'css',
+    'html',
     done
   );
 });
